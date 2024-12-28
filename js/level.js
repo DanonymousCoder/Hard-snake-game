a54b1653afe64572dfe3b589bcc24f4c26113838
Ridwanullah <aladebaruwa@gmail.com>
2024-12-28 16:22:17 +0100

    WIP

--- js/level.js
+++ js/level.js
@@ -1,3 +1,4 @@
 let user = document.getElementById("user");
+let easy
 
 user.innerHTML = localStorage.getItem("userNameInput")
--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let userName = document.getElementById("username");
 let continueBtn = document.getElementById("continue-btn");
-let txt = "Welcome to the <span>hardest</span> snake game on <span>EARTH</span>";
+let txt = `Welcome to the <span>hardest</span> snake game on <span>EARTH</span>`;
 let i = 0;
 let speed = 50;
 
-function typewriter() {
-    
+window.addEventListener("DOMContentLoaded", typewriter);
+
+function typewriter(){
+    if (i < txt.length) {
+        document.getElementById("type").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);
+    }
 }
+
 
 continueBtn.addEventListener("click", () => {
 
