82cc3abd30a04d47b1174201d887460e93034d6c
Ridwanullah <aladebaruwa@gmail.com>
2024-12-29 22:39:54 +0100

    Cooking 😮‍💨

--- js/game.js
+++ js/game.js
@@ -1,1 +1,23 @@
 const canvas = document.getElementById("gameCanvas");
+
+let snake = [
+    {
+        x: 10,
+        y: 10
+    }
+];
+
+function draw() {
+    canvas.innerHTML = " ";
+
+    // Draw snake
+    const drawSnake = () => {
+        snake.forEach((segment) => {
+            const snakeElement = document.createElement("div");
+            snakeElement.classList.add("snake");
+            snakeElement.style.gridRowStart = segment.y;
+            snakeElement.style.gridColumnStart = segment.x;
+            canvas.appendChild(snakeElement);
+        })
+    };
+}
