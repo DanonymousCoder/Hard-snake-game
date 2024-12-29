41b976427abde4d280c631677b7612893052f012
Ridwanullah <aladebaruwa@gmail.com>
2024-12-29 17:50:42 +0100

    rewriting some codes

--- js/game.js
+++ js/game.js
@@ -16,20 +16,5 @@ function setDirection(event) {
         direction = "LEFT";
     } else if (event.keycode == 39 && direction != "LEFT"){
         direction = "RIGHT";
-    } else if (event.keycode == 38 && direction != "DOWN") {
-        direction = "UP";
-    } else if (event.keycode == 40 && direction != "UP") {
-        direction = "DOWN";
     }
 }
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i == 0) ? "green" : "white";
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-    
