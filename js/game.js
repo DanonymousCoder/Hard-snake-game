b3af7822feb10b35604b9cf4bcb61ecc8f239640
Ridwanullah <aladebaruwa@gmail.com>
2024-12-29 22:53:46 +0100

    there is a bug

--- js/game.js
+++ js/game.js
@@ -7,21 +7,35 @@ let snake = [
     }
 ];
 
+let food = generateRandomFood();
+
 function draw() {
     canvas.innerHTML = " ";
 
     // Draw snake
-    const drawSnake = () => {
-        snake.forEach((segment) => {
-            const snakeElement = document.createElement("div");
-            snakeElement.classList.add("snake");
-            snakeElement.style.gridRowStart = segment.y;
-            snakeElement.style.gridColumnStart = segment.x;
-            canvas.appendChild(snakeElement);
-        })
-    };
+    drawSnake();
 }
+
+
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = document.createElement("div");
+        snakeElement.classList.add("snake");
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
+        canvas.appendChild(snakeElement);
+    })
+};
 
 
 // Draw testing
 draw();
+
+
+// function to draw the snake food
+
+function snakeFood() {
+    const foodElement = document.createElement("div");
+    foodElement.classList.add("food");
+    foodElement.style.gridRowStart = 5;
+}
