f25a6a74793e096320187e86bdf586987d2d4c9e
Ridwanullah <aladebaruwa@gmail.com>
2024-12-29 21:27:39 +0100

    Cooking 🔥

--- js/game.js
+++ js/game.js
@@ -1,18 +1,1 @@
 const canvas = document.getElementById("gameCanvas");
-const context = canvas.getContext("2d");
-
-const box = 20;
-let snake = [];
-snake[0] = {
-    x: 9 * box,
-    y: 10* box
-}
-
-let direction;
-document.addEventListener("keydown", setDirection);
-
-function setDirection(event) {
-    if (event.keycode == 37 && direction != "RIGHT") {
-        direction = "LEFT";
-    } else if (event.
-}
