4590aae580cdfcf0066c541847966197eea0c860
Ridwanullah <aladebaruwa@gmail.com>
2024-12-30 15:31:54 +0100

    WIP

--- js/game.js
+++ js/game.js
@@ -7,7 +7,8 @@ let snake = [
     }
 ];
 
-// let food = generateRandomFood();
+
+let food = generateRandomFood();
 
 function draw() {
     canvas.innerHTML = " ";
@@ -24,7 +25,8 @@ function draw() {
         })
     };
 
-    drawSnake()
+    drawSnake();
+    snakeFood();
 }
 
 
@@ -40,4 +42,15 @@ function snakeFood() {
     let foodElement = document.createElement("div");
     foodElement.classList.add("food");
     foodElement.style.gridRowStart = 5;
+    canvas.appendChild(foodElement);
+}
+
+
+function generateRandomFood() {
+    let x = Math.floor(Math.random() * 20) + 1;
+    let y = Math.floor(Math.random() * 20) + 1;
+    return {
+        x,
+        y
+    }
 }
